dcc357a2a369fde086116db26ffda5465df15ce6
ComputerAided <ComputerAided@users.noreply.github.com>
2016-05-27 21:13:51 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+def hello:
+  return(render_template("main.html", **templatedata)
+  
+if __name__ == "__main__":
+  app.run(debug = True, host = '10.0.0.28', port = 70)
